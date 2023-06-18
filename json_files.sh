fb36fcbe72b4dcefe94e1ecbcbf6209172fc9056
SanchoFdz <santiago.fernandezdelcastillo@gmail.com>
2023-06-18 16:48:27 -0600

    raw EDA and sh files for Danone 2023 Hackathon

---
+++ json_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl https://challenges-asset-files.s3.us-east-2.amazonaws.com/Events/Danone+Hackathon+Mexico/data/train_products.json >> ./train_products.json
+
+curl https://challenges-asset-files.s3.us-east-2.amazonaws.com/Events/Danone+Hackathon+Mexico/data/test_products.json >> ./test_products.json
+
